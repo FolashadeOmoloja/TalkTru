e63b67a0b28b07b61f805184864f6d8210f8b0be
FolashadeOmoloja <omolojashade@gmail.com>
2024-02-16 16:13:48 +0100

    auth

--- app/(auth)/sign-up/layout.tsx
+++ app/(auth)/sign-up/layout.tsx
@@ -1,4 +1,26 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { PT_Sans } from "next/font/google";
+
 export const metadata = {
     title: 'TalkTru',
     description: 'A social media platform that allows youths to express themselve, connect and build productive communities'
 }
+
+const pt_sans= PT_Sans({ subsets: ["latin"], weight:['400','700'] });
+ const RootLayout = ({
+    children} : 
+    {
+        children: React.ReactNode
+    }
+ ) => {
+           <ClerkProvider>
+              <html lang="en">
+                  <body className={pt_sans.className}>
+                    {children}
+                   </body>
+              </html>
+           </ClerkProvider>
+}
+
+
+export default RootLayout
