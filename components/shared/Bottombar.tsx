f11d1d7db491c2eeadd02ac3948fcf4508857014
FolashadeOmoloja <omolojashade@gmail.com>
2024-02-17 17:21:48 +0100

    bottombar

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+"use client"
+import { sidebarLinks } from '@/constants'
+import Link from 'next/link'
+import Image from 'next/image'
+import { usePathname, useRouter } from 'next/navigation'
+import LogoutButton from '../elements/LogoutButton'
+
 
 const Bottombar = () => {
+  const router = useRouter()
+  const pathname = usePathname()
   return (
-    <div>Bottombar</div>
+    <section className='bottombar'>
+         <div className='bottombar_container'>
+         {
+              sidebarLinks.map((link)=>{
+
+                const isActive = (pathname.includes(link.route) && link.route.length > 1 || pathname === link.route)
+                return (
+                  <Link 
+                     href={link.route} 
+                     key={link.label}
+                     className={`bottombar_link ${isActive && 'bg-primary-500'}`}
+                     >
+                      <Image
+                      src={link.imgURL}
+                      alt={link.label}
+                      width={24}
+                      height={24}
+                      />
+                    <p className='text-subtle-medium text-light-1 max-sm:hidden'>
+                      {link.label.split(/\s+/)[0]} 
+                      {/*this cuts off the words that may enter a new line!!!*/}
+                    </p>
+                  </Link>
+              )}
+              )}
+         </div>
+    </section>
   )
 }
 
