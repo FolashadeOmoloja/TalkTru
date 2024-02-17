ddb729057b4c8c0e901b2750929653a214bcf238
FolashadeOmoloja <omolojashade@gmail.com>
2024-02-17 11:52:35 +0100

    Topbar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import Link from 'next/link'
+import Image   from 'next/image'
 
 const Topbar = () => {
   return (
-    <div>Topbar</div>
+    <nav className='topbar'>
+        <Link href='/' className='flex flex-col items-center '>
+                <Image src='/logo2.png' alt={'logo'} width={32} height={32}/>  
+                <p className={` text-heading3-bold text-[#f3f3f5] max-xs:hidden mt-[-7px]`}>TalkTrue</p>
+        </Link>
+    </nav>
   )
 }
 
