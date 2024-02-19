4da596f2b8bbd6b883e90ce8205f4972087b39ce
FolashadeOmoloja <omolojashade@gmail.com>
2024-02-19 15:56:10 +0100

    onboarding

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -20,7 +20,7 @@ async function Page() {
    return(
     <main className="mx-auto max-w-3xl flex flex-col justify-start px-10 py-20">
         <h1 className="head-text">Onboarding</h1>
-        <p className="mt-3 text-base-regular text-light-2 ">You're just a few steps from becoming a Truetalker, <br /> complete your profile now</p>
+        <p className="mt-3 text-base-regular text-light-2 ">You're just a few steps from becoming a Truetalker, <br /> Complete your profile now</p>
         <section className="mt-9 bg-dark-2 p-10">
              <AccountProfile user={userData} btnTitle="Continue"/>
         </section>
